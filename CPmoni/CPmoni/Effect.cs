8e2bde4f1b196f64677ac61d07ec55f051d93553
Dan <danielsmilek@seznam.cz>
2026-06-12 18:42:27 +0200

    Update Effect.cs

--- CPmoni/CPmoni/Effect.cs
+++ CPmoni/CPmoni/Effect.cs
@@ -1,5 +1,11 @@
 ﻿class Effect{
 
     public string Jmeno { get; set; }
+    public int DelkaRoundu { get; set; }
 
-}
+    public Effect(string jmeno, int delkaroundu)
+    {
+        Jmeno = jmeno;
+        DelkaRoundu = delkaroundu;
+    }
+ }
